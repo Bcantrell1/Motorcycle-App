e6cb2d4ea8ed9618e0ca6e075572068e5c97df69
Brian Cantrell <bribri546@gmail.com>
2020-02-23 11:10:44 -0700

    small app.php change

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://blooming-inlet-67828.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
